e8a64fcac663df3baa4602079a2ab9640987a311
Erisvaldo <erisvaldosilvadesousa38@gmail.com>
2021-12-28 16:58:21 -0300

    Modal Implementado

--- src/php/home.php
+++ src/php/home.php
@@ -15,6 +15,29 @@ include_once "validation.php";
 </head>
 <body>
     <main>
+       <div id="popup">
+           <header id="header">
+             <a class="close" onclick="popupToggle();"> <img src="../assets/cancel.png" alt="Icone para Sair"> </a>
+           </header>
+           <div id="container"> <!--- Modal --->
+               <img src="../assets/email.png" alt="Icone de um Email">
+               <div class="title">
+                  <h2> Resgate Sua senha </h2>
+               </div>
+               <div class="text">
+                  <p id="message"> Digite logo abaixo o seu e-mail e a nova senha que você deseja que faça parte de sua conta. </p>
+               </div>
+               <form action="" method="post">
+                 <div class="input-box">
+                     <input type="email" name="mail" id="email" placeholder="Enter email">
+                     <input type="password" name="password" id="password" placeholder="Enter New Password">
+                 </div>
+                 <div id="button">
+                    <button type="submit" name="reset_password" class="reset"> CHANGE PASSWORD </button>
+                 </div>
+               </form>
+          </div>
+       </div>
       <header>
         <div class="text">
             <h1 class="title"> Login </h1>
@@ -26,11 +49,11 @@ include_once "validation.php";
            <input type="text" name="password" class="campos" placeholder="Senha">
            <button type="submit" class="btn" name="login"> Enviar </button>
            <button type="submit" class="btn" name="register"> Cadastrar </button>
-           <button type="submit" name="reset_password" class="btn"> Resgatar Conta </button>
-           <?php if(!empty($msg)): ?> <p> <?= $msg ?> </p> <?php endif; ?>
+           <a id="btn" class="btn" onclick="popupToggle();"> Resgatar Conta </a>
         </form>
       </div> 
      <footer> <h5> © Erisvaldo Silva </h5> </footer>
     </main>
+  <script src="../js/modal.js"> </script>
 </body>
 </html>
